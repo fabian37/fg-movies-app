da35b420aab93858621931d29ac3cdb650030f8e
Fabian Gomez <fabian37gomez@gmail.com>
2023-02-04 13:55:46 -0500

    feat: add navbar styles

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,10 +1,54 @@
 import React from 'react';
-import styles from './Loader.module.css';
+import styled from 'styled-components';
+
+const LoaderContainer = styled.div`
+	width: 100%;
+	height: 100vh;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+`;
+
+const Loading = styled.div`
+	position: relative;
+	width: 40px;
+	height: 40px;
+	border-radius: 50%;
+
+	&::before,
+	&::after {
+		content: '';
+		position: absolute;
+		border-radius: inherit;
+	}
+
+	&::before {
+		width: 100%;
+		height: 100%;
+		background-image: linear-gradient(0deg, #7a51ff 0%, #5153ff 100%);
+		animation: loader 0.5s infinite linear;
+	}
+
+	&::after {
+		width: 85%;
+		height: 85%;
+		background-color: #2c2c2c;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+	}
+
+	@keyframes loader {
+		to {
+			transform: rotate(360deg);
+		}
+	}
+`;
 
 export const Loader = () => {
 	return (
-		<div className={styles.loaderContainer}>
-			<div className={styles.loader}></div>
-		</div>
+		<LoaderContainer>
+			<Loading></Loading>
+		</LoaderContainer>
 	);
 };
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,15 +1,84 @@
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
-import styles from './Search.module.css';
+import styled from 'styled-components';
+
+const Form = styled.form`
+	align-items: center;
+	display: flex;
+	justify-content: center;
+	margin: 1em 0;
+	width: 100%;
+`;
+const SearchBox = styled.div`
+	align-items: center;
+	display: flex;
+	justify-content: center;
+`;
+const InputWrapper = styled.div`
+	--width-of-input: 100%;
+	--border-height: 1px;
+	--border-before-color: rgba(221, 221, 221, 0.39);
+	--border-after-color: #5153ff;
+	--input-hovered-color: #69696b21;
+	position: relative;
+	min-width: 250px;
+	width: var(--width-of-input);
+`;
+const Input = styled.input`
+	color: #fff;
+	font-size: 1rem;
+	background-color: transparent;
+	width: 100%;
+	box-sizing: border-box;
+	padding-inline: 0.5em;
+	padding-block: 0.7em;
+	border: none;
+	border-bottom: var(--border-height) solid var(--border-before-color);
+
+	&:hover {
+		background: var(--input-hovered-color);
+	}
+
+	&:focus {
+		outline: none;
+	}
+
+	&:focus ~ span {
+		width: 100%;
+	}
+`;
+const Span = styled.span`
+	position: absolute;
+	background: var(--border-after-color);
+	width: 0%;
+	height: 2px;
+	bottom: 0;
+	left: 0;
+	transition: 0.3s;
+`;
+const Btn = styled.button`
+	padding-right: 1em;
+`;
+const StyledFontAwesomeIcon = styled(FontAwesomeIcon)`
+	color: white;
+	font-size: 1.5rem;
+	cursor: pointer;
+`;
 
 export const Search = () => {
+	
 	return (
-		<form className={styles.searchContainer}>
-			<div className={styles.searchBox}>
-				<input className={styles.searchInput} type="text" />
-				<button className={styles.searchButton} type="submit">
-					Search
-				</button>
-			</div>
-		</form>
+		<Form>
+			<SearchBox>
+				<Btn type="submit">
+					<StyledFontAwesomeIcon icon={faMagnifyingGlass} />
+				</Btn>
+				<InputWrapper>
+					<Input placeholder="What are you looking for?" required type="text" />
+					<Span></Span>
+				</InputWrapper>
+			</SearchBox>
+		</Form>
 	);
 };
